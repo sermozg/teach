018bdaeeb29c96b6047c74f316511dba780fefa5
sermozg <admin@mozgovyi.in.ua>
2012-11-22 13:35:36 +0200

    add z351

--- com/mozgovyi/teach/z351.java
+++ com/mozgovyi/teach/z351.java
@@ -6,14 +6,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 
-public class z315 {
+public class z351 {
 
 	/**
 	 * @param args
-	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		BufferedReader in = null; 
@@ -21,15 +18,12 @@ public class z315 {
 		try { 
 			in = new BufferedReader(new FileReader("input.txt")); 
 		 	out = new PrintWriter(new FileWriter("output.txt")); 
-		 	long n = Integer.parseInt(in.readLine());
-		 	double s = 0.0;
-		 	if (n>0) {
-			 	for (int i = 1;i<=n;i++){
-				 	s += java.lang.Math.pow(i, 2); 
-			 	} 		
-		 	}
-		 	NumberFormat form = new DecimalFormat("0");
-		 	out.println(form.format(s));
+		 	int n = Integer.parseInt(in.readLine());
+		 	int f = 1;
+		 	for (int i = 1;i<=n;i++){
+			 	f *= i; 
+		 	} 		
+		 	out.print(f);
 		} finally { 
 			 if (in != null) { in.close(); } 
 			 if (out != null) { out.close(); } 
